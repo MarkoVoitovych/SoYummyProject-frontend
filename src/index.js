98acd2fc54025ea881853a63979ecf3ab5a46aa6
MarkoVoitovych <searchangeos@gmail.com>
2023-04-26 16:17:23 +0300

    deploy file added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="SoYummyProject-frontend">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
